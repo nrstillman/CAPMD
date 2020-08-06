0b5fc8bc0d0c4621606574fd16913057fc3c218d
Mike Talbot <michael.talbot89@gmail.com>
2020-08-06 13:00:43 +0100

    Updated python scripts to detect any circle colliging with any specififed polygon - includes test for simple rectangle and returns id of cells collided with

--- capmd_python_test.py
+++ capmd_python_test.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 #import matplotlib.pyplot as plt
-#import numpy as np
+import numpy as np
 import Debug.pycapmd as pycapmd # this can be changed to build.pycapmd when availiable
 import parameters as p
 import plotter
@@ -9,7 +9,7 @@ resolution = (500, 500)
 parameterFile = "parameters.json"
 params = p.paramsFromFile(pycapmd.Parameters(), parameterFile)
 
-sim = pycapmd.simulation(params)
+sim = pycapmd.simulation(params) # interface
 sim.initialise()
 
 p = sim.getParticle(0)
@@ -18,20 +18,32 @@ p.getPosition()
 N = sim.popSize()
 NB = sim.getBoundarySize()
 a = range(NB, N+NB)
-parts = pycapmd.VectorInt(a)
+part_ids = pycapmd.VectorInt(a)
 
 N = sim.popSize()
 #plotter.testDrawWindow("TEST")
 print("[INFO] - Animation Started")
 end = False
-for t in tqdm(range(2000)):
+
+rect = None
+for t in tqdm(range(1000)):
     #break
     if int(t)%100 == 0:
         sim.populationDynamics(100)
+        rect = np.random.randint(0,resolution[0],(2,2)) # generates a random rect of the size of the screen
+    num_particles = sim.popSize()
+    index = list(range(NB, num_particles + NB))
+    #part_ids = []
+    #for idx in index:
+        #part_ids.append(sim.getParticle(idx))
+        #print(sim.getParticle(idx))
+    #print(part_ids)
+    part_ids = pycapmd.VectorInt(index)
     sim.move(t) # n is current time step. weird
-    part_pos = sim.getPopulationPosition(parts)
-    part_rad = sim.getPopulationRadius(parts)
-    plotter.animPlot(part_pos, part_rad, params, resolution)
+    part_pos = sim.getPopulationPosition(part_ids)
+    part_rad = sim.getPopulationRadius(part_ids)
+    kill_list = plotter.animPlot(part_pos, part_rad, index, params, resolution, rect)
+    #print(kill_list)
     plotter.checkPause("p")
     end = plotter.cleanExit("q")
     if end:
--- plotter.py
+++ plotter.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import collision
 
 events = [i for i in dir(cv2) if 'EVENT' in i]
 # Default values for drawing in window
@@ -7,20 +8,29 @@ drawing = False # true when mouse down
 mode = True # may change this frome T/F to an item from a list depends what is needed
 ix,iy = -1,-1 # initial mouse down coord for rects 
 
-def animPlot(positions, radii, params, res = (1280, 780)):
+def animPlot(positions, radii, part_ids, params, res = (1280, 780), rect = None):
     animFrame = np.ones([res[0], int(res[0]*params.Lx/params.Ly), 3])
     #print(animFrame)
-    for (position, radius) in zip(positions, radii): 
+    kill_list = []
+    if rect is not None:
+        cv2.rectangle(animFrame, (rect[0,0],rect[0,1]), (rect[1,0],rect[1,1]), (255,255,0), -1)
+    for (position, radius, ids) in zip(positions, radii, part_ids): 
+        coll = 0
         pixel = convertPoint2Pixel(position, (params.Lx, params.Ly), res)
         #print(radius)
         radius = int(20*radius)
+        if rect is not None:
+            (coll,_) = collision.circle_intersect_simple_rect(rect, (pixel,radius))
         #radius, _ = convertPoint2Pixel((radius,0), (params.Lx, params.Ly), res)
-        cv2.circle(animFrame, pixel, radius, (0,0,1))
-    
+        cv2.circle(animFrame, pixel, radius, (0,coll,1))
+        cv2.putText(animFrame, str(ids), pixel, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,0))
+        if coll:
+            kill_list.append(ids)
+        
     cv2.imshow('Simulation', animFrame)
     cv2.namedWindow('Simulation', cv2.WINDOW_AUTOSIZE)
     cv2.setWindowProperty('Simulation', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    return None
+    return kill_list
     
 def convertPoint2Pixel(pos, L, res):
     Lx,Ly = L
@@ -79,3 +89,40 @@ def testDrawWindow(windowName):
         elif key == 32:
             print("exit")
             break
+
+        
+def testIntersection():
+    
+    size = 1000 
+    circs = 500
+    animFrame = np.ones([size, size, 3])
+    rect = np.random.randint(0,size,(2,2))
+    cv2.rectangle(animFrame, (rect[0,0],rect[0,1]), (rect[1,0],rect[1,1]), (255,255,0), -1)
+    for _ in range(circs):
+        pxls = np.random.randint(5,size-5,2)
+        pxls = (pxls[0], pxls[1])
+        r = np.random.randint(5,25)
+        (coll,var) = collision.circle_intersect_simple_rect(rect, (pxls,r))
+        if var is None:
+            color = (0,0,1)
+        else:
+            color = (1,0,1)
+        """
+        elif var == 0:
+            color = (1,0,0) # g
+        elif var == 1:
+            color = (0,1,0) # b
+        elif var == 2:
+            color = (1,0,1) # y
+        elif var == 3:
+            color = (1,1,1) # b
+        elif var == 5:
+            color = (0,0,0) # w
+        """
+                
+        cv2.circle(animFrame, pxls, r, color, 5)
+    cv2.imshow('Test_Simulation', animFrame)
+    cv2.namedWindow('Test_Simulation', cv2.WINDOW_AUTOSIZE)
+    cv2.setWindowProperty('Test_Simulation', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    
+#testIntersection()
